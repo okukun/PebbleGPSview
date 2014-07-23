b0b8670d8688d2b138bc1031dd8a86896f255972
okukun <lordpuk@gmail.com>
2014-07-23 01:48:24 -0700

    push_test

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ console.log('Simply.js Geo');
 
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
-  var lat = coords.latitude;
-  var lon = coords.longitude;
-  /*var lat_g = lat*60+" "+lat*60*60-lat*60;*/
-  /*var lon_g = lon*60+" "+lon*60*60-lon*60; */
+  var lat = coords.latitude.toString().split(".");
+  var lon = coords.longitude.toString().split(".");
+  var lat_d = parseInt(lat[0]);
+  var lat_ht = parseInt("0."+lat[1]);
+  var lat_h = (lat_ht*60).substring(0,3);
+  var lon_d = parseInt(lon[0]);
+  var lon_ht = parseInt("0."+lon[1]);
+  var lon_h = (lon_ht*60).substring(0,3);
   simply.setText({ title:'GeoPosition:' }, true);
-  simply.text({ body:lat+"\n"+lon });
+  simply.text({ body:lat_d+"° "+lat_h+"\n"+lon_d+"° "+lon_h });
 });
